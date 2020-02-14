752d936065bbd1d834c993d582ca491cd5104456
Loralee Dawn <loraleedawn93@gmail.com>
2020-02-13 20:28:12 -0800

    burger

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-	port: '3306',
-	host: 'localhost',
-	user: 'root',
-	password: 'password',
-	database: 'burgers_db'
-});
+let connection;
 
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+
+	connection = mysql.createConnection({
+		port: '3306',
+		host: 'localhost',
+		user: 'root',
+		password: 'password',
+		database: 'burgers_db'
+	});
+
+}
+
+connection.connect(function (err) {
 	if (err) {
 		return console.error('error: ' + err.message);
 	}
